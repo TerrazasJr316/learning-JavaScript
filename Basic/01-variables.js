945d5f1c2f1a08cfcbf4edf6fb0946e74ff2889b
Josue Terrazas Mendoza <160078635+TerrazasJr316@users.noreply.github.com>
2024-07-11 05:03:51 +0000

    base JavaScript

---
+++ Basic/01-variables.js
@@ -0,0 +1,26 @@
+// var
+
+var helloWrold = "Hello JavaScript"
+console.log(helloWrold)
+
+// var fue la forma original de declarar variables en JavaScript
+
+helloWrold = "Hola de nuevo, JavaScript"
+console.log(helloWrold)
+
+// let
+
+let helloWrold2 = "Hola JavaScript"
+console.log(helloWrold2)
+
+helloWrold2 = "Hola de nuevo JavaScript"
+console.log(helloWrold2)
+
+// const
+
+const helloWrold3 = "Hola JavaScript 3"
+console.log(helloWrold3)
+
+// Error
+// helloWrold3 = "Hola de nuevo JavaScript 3"
+// console.log(helloWrold3)
