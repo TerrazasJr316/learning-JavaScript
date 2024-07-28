0a45f80ce124581d73a849b6568f07c0cd25f53e
Josue Terrazas Mendoza <160078635+TerrazasJr316@users.noreply.github.com>
2024-07-28 08:16:25 -0600

    Clase 2

--- Basic/03-bigginner-exercises.js
+++ Basic/03-bigginner-exercises.js
@@ -1,18 +1,99 @@
 // 1. Escribe un comentario en una línea
 
-// Este es un comentario en una línea bienvenido soy Josué Terrazas Mendoza
+// Bienvenido soy Josué Terrazas Mendoza
 
 // 2. Escribe un comentario en varias líneas
 
+/*
+Soy ingeniero en sistemas computacionales
+estudiante del Tecnológico de Estudios
+Superiores de Jilotepec
+*/
+
 // 3. Declara variables con valores asociados a todos los datos de tipo primitivos
+
+let petName = "Muñeca"
+let girlfriends = 5
+let score = 80.90
+
+let isTall = true
+let isSmall = false
+
+let mtypeofyValueUndefined
+
+let mtypeofyValueNull = null
+
+let mtypeofyValueSymbol = Symbol("Tengo ojos")
+
+let myValueBigInt = 2045702348767682637065873486036036782386283036868375028
 
 // 4. Imprime por consola el valor de todas las variables
 
+console.log(petName)
+
+console.log(girlfriends)
+console.log(score)
+
+console.log(isTall)
+console.log(isSmall)
+
+console.log(myValueUndefined)
+
+console.log(myValueNull)
+
+console.log(myValueBigInt)
+
 // 5. Imprime por consola el tipo de todas las variables
+
+console.log(typeof petName)
+
+console.log(typeof girlfriends)
+console.log(typeof score)
+
+console.log(typeof isTall)
+console.log(typeof isSmall)
+
+console.log(typeof myValueUndefined)
+
+console.log(typeof myValueNull)
+
+console.log(typeof myValueBigInt)
 
 // 6. A continuación, modifica los valores de las variables por otros del mismo tipo
 
+let petName = "Toby"
+
+let girlfriends = 3
+let score = 50.63
+
+let isTall = false
+let isSmall = true
+
+let mtypeofyValueUndefined
+
+let mtypeofyValueNull = null
+
+let mtypeofyValueSymbol = Symbol("Tengo dientes")
+
+let myValueBigInt = 95300038945024802486927834592934752430234873040234836349209434702
+
 // 7. A continuación, modifica los valores de las variables por otros de distinto tipo
+
+let petName = null
+
+let girlfriends = true
+let score = Symbol("Azul colo favorito")
+
+let isTall = 1.73
+let isSmall
+
+let mtypeofyValueUndefined = "Estudiante"
+
+let mtypeofyValueNull = 8734072307304850397409723475467204602437032745092374920
+
+let mtypeofyValueSymbol = 21
+
+let myValueBigInt = false
 
 // 8. Declara constantes con valores asociados a todos los tipos de datos primitivos
 
